841aa77f7d02b710e6824eec5c6a0d79605f6937
dineshchandrasekaren <dineshchandrasekaren@gmail.com>
2023-08-26 18:58:46 +0530

    partialy completed

---
+++ script.js
@@ -0,0 +1,130 @@
+let newsLaunchesData = [
+  {
+    image:
+      "https://lh3.googleusercontent.com/51moDkmVWptBFXRReuACKocS_fsOoYtfT-C78onEUKeRr7ky4l_ZsuZMK2AnE7-MPRbLzcKTAIFmNyyN7ACo0-eygmXaGeR0ItYtRcYNE8-_L8RHqkA=w960",
+    title: "Material Design at Google I/O",
+    desc: "See the full list of Material talks and tutorials launching at I/O 2023",
+  },
+  {
+    image:
+      "https://lh3.googleusercontent.com/aiJojq_gpPLqLhU0PZH6Dog-OjIMti_hiclF_6oqxucGuvywn_AEtu2Qrk2f4WxEhRWMjXkz8qXpTCGoj5TZ0DUCEZ5UIXnXTSIu_7ndg2dTNmQbdkCO=w960",
+    title: "New: Large screen guidance",
+    desc: "Get started adapting your layouts for a range of window size classes and hardware",
+  },
+  {
+    image:
+      "https://lh3.googleusercontent.com/mLGakWFjdX3zGBIhCUWqzjlj3gUXOY8hOed_ISzomlpNmjKWUiqCFyNGGAZFneVaK30v4GUpjn7lyIsI2Z7KPvMKrYkumzBr2nwbVe3BQKRkpam3sA=w960",
+    title: "Figma design kit for M3",
+    desc: "Jumpstart your M3 designs with the complete library of ready-made components",
+  },
+];
+
+let designSystemData = [
+  {
+    image:
+      "https://lh3.googleusercontent.com/51moDkmVWptBFXRReuACKocS_fsOoYtfT-C78onEUKeRr7ky4l_ZsuZMK2AnE7-MPRbLzcKTAIFmNyyN7ACo0-eygmXaGeR0ItYtRcYNE8-_L8RHqkA=w960",
+    title: "M3 for Jetpack Compose",
+    desc: "With the latest stable release, use Jetpack Compose to build a complete M3 Android app",
+  },
+  {
+    image:
+      "https://lh3.googleusercontent.com/aiJojq_gpPLqLhU0PZH6Dog-OjIMti_hiclF_6oqxucGuvywn_AEtu2Qrk2f4WxEhRWMjXkz8qXpTCGoj5TZ0DUCEZ5UIXnXTSIu_7ndg2dTNmQbdkCO=w960",
+    title: "New component: M3 carousel",
+    desc: "An expressive new component for displaying scrollable collections of content",
+  },
+  {
+    image:
+      "https://lh3.googleusercontent.com/mLGakWFjdX3zGBIhCUWqzjlj3gUXOY8hOed_ISzomlpNmjKWUiqCFyNGGAZFneVaK30v4GUpjn7lyIsI2Z7KPvMKrYkumzBr2nwbVe3BQKRkpam3sA=w960",
+    title: "Back navigation & gestures",
+    desc: "Explore new motion and gesture capabilities for smooth navigation across screens",
+  },
+  {
+    image:
+      "https://lh3.googleusercontent.com/mLGakWFjdX3zGBIhCUWqzjlj3gUXOY8hOed_ISzomlpNmjKWUiqCFyNGGAZFneVaK30v4GUpjn7lyIsI2Z7KPvMKrYkumzBr2nwbVe3BQKRkpam3sA=w960",
+    title: "Motion system updates",
+    desc: "Get started applying motion, from ready-to-use transition patterns to updated guidance",
+  },
+  {
+    image:
+      "https://lh3.googleusercontent.com/mLGakWFjdX3zGBIhCUWqzjlj3gUXOY8hOed_ISzomlpNmjKWUiqCFyNGGAZFneVaK30v4GUpjn7lyIsI2Z7KPvMKrYkumzBr2nwbVe3BQKRkpam3sA=w960",
+    title: "Material Theme Builder",
+    desc: "Easily migrate or get started with the M3 color system and dynamic color",
+  },
+  {
+    image:
+      "https://lh3.googleusercontent.com/mLGakWFjdX3zGBIhCUWqzjlj3gUXOY8hOed_ISzomlpNmjKWUiqCFyNGGAZFneVaK30v4GUpjn7lyIsI2Z7KPvMKrYkumzBr2nwbVe3BQKRkpam3sA=w960",
+    title: "New color guidance for tones",
+    desc: "Updates include tone-based surface colors, contrast modes, and fixed accent colors",
+  },
+];
+let resourcesData = [
+  {
+    title: "Material blog",
+    desc: "News, tutorials, and inspiration from the Material team",
+  },
+  {
+    title: "Figma design kit",
+    desc: "Customizable styles and components to help you design with Material 3",
+  },
+  {
+    title: "Get started",
+    desc: "Guides, videos, and tools to start building with Material",
+  },
+  {
+    title: "Develop",
+    desc: "Code and developer documentation for Android, Flutter, and the Web",
+  },
+];
+
+let sectionRender = [
+  {
+    node: document.getElementById("news-launches"),
+    data: newsLaunchesData,
+    heading: "News & launches",
+  },
+  {
+    node: document.getElementById("designSystem"),
+    data: designSystemData,
+    heading: "Design system updates",
+  },
+  {
+    node: document.getElementById("resources"),
+    data: resourcesData,
+    heading: "Resources",
+  },
+];
+sectionRender.forEach(({ node, data, heading }) => {
+  let section = node;
+  section.innerHTML = `<h2 class="section-title">${heading}</h2>`;
+  let cardContainer = document.createElement("div");
+  if (heading === "Resources") {
+    cardContainer.className = "card-container col-2";
+  } else {
+    cardContainer.className = "card-container col-3";
+  }
+
+  data.forEach((cardDetail) => {
+    let card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `
+    ${
+      cardDetail.image
+        ? `<img
+          src="${cardDetail.image}"
+          alt="${cardDetail.title}"
+          class="card-img"
+        />`
+        : ""
+    }
+  
+            <div class="card-info">
+              <h3>${cardDetail.title}</h3>
+              <p>
+                ${cardDetail.desc}
+              </p>
+            </div>`;
+    cardContainer.appendChild(card);
+  });
+  section.appendChild(cardContainer.cloneNode(true));
+  cardContainer.innerHTML = "";
+});
